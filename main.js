c8630aff2ba47a123ecb491f7193fddf105fd16d
areum-ko <79952747+areum-ko@users.noreply.github.com>
2021-04-14 22:40:33 +0900

    JS: Remove active from nav

--- main.js
+++ main.js
@@ -54,14 +54,23 @@ arrowUp.addEventListener('click',()=>{
 const workBtnContainer = document.querySelector('.work__categories');
 const projectContainer = document.querySelector('.work__projects');
 const projects = document.querySelectorAll('.project');
-workBtnContainer.addEventListener('click',(e)=>{
+workBtnContainer.addEventListener('click',e => {
     const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
     if(filter == null){
         return;
     }
+    
+    //Remove selection from the previous item and select the new one
+    const active = document.querySelector('.category__btn.selected');
+    active.classList.remove('selected');
+    const target = e.target.nodeName === 'BUTTON' ?
+    e.target : e.target.parentNode;
+    target.classList.add('selected');
+
+
     projectContainer.classList.add('anim-out');
     setTimeout(() => {
-        projects.forEach((project)=>{
+        projects.forEach(project => {
             console.log(project.dataset.type);
             if(filter =='*' || filter == project.dataset.type){
                 project.classList.remove('invisible');
@@ -70,7 +79,6 @@ workBtnContainer.addEventListener('click',(e)=>{
             }
         });
         projectContainer.classList.remove('anim-out');
-
     },300);
     console.log(filter);
 });
